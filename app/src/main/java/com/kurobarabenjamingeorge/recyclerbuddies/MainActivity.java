42b63ff6c5b850181027845cdf2aaaa9e8f26996
George Benjamin <msdcconnect@gmail.com>
2018-06-21 13:50:23 +0100

    Populated buddy list from array.xml

--- app/src/main/java/com/kurobarabenjamingeorge/recyclerbuddies/MainActivity.java
+++ app/src/main/java/com/kurobarabenjamingeorge/recyclerbuddies/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
@@ -17,10 +18,19 @@ public class MainActivity extends AppCompatActivity implements BuddyListAdapter.
     private ArrayList<Buddy> buddies;
     private BuddyListAdapter adapter;
 
+    private String[] buddyNames;
+    private String[] buddyDesc;
+    private int[] buddyImg = {R.drawable.bambie, R.drawable.bart, R.drawable.donald,
+    R.drawable.goofy, R.drawable.homer, R.drawable.jerry, R.drawable.micky, R.drawable.simba,
+    R.drawable.tom};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buddyNames = getResources().getStringArray(R.array.buddy_names);
+        buddyDesc = getResources().getStringArray(R.array.buddy_desc);
 
         buddyList = (RecyclerView) findViewById(R.id.buddy_recycler_view);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
@@ -28,7 +38,11 @@ public class MainActivity extends AppCompatActivity implements BuddyListAdapter.
         buddyList.setHasFixedSize(true);
 
         buddies = new ArrayList<>();
-        buddies.add(new Buddy("Bambi", "Bambi is a boy", R.drawable.bart));
+        //buddies.add(new Buddy("Bambi", "Bambi is a boy", R.drawable.bart));
+        for(int i = 0; i < buddyNames.length; i++){
+            buddies.add(new Buddy(buddyNames[i], buddyDesc[i], buddyImg[i]));
+            //Log.i("Image res", Integer.toString(buddyImg[i]));
+        }
 
         adapter = new BuddyListAdapter(buddies, this, this);
 
